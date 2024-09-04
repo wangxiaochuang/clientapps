2beff933467299c5ade7ac02dd5d90fe4a149187
wangxiaochuang <932698529@qq.com>
2024-09-04 21:49:03 +0800

    feature: support comments load

--- hackernews/src/main.rs
+++ hackernews/src/main.rs
@@ -2,14 +2,17 @@
 
 use dioxus::prelude::*;
 use dioxus_logger::tracing::{info, Level};
+use hackernews::App;
 
-#[derive(Clone, Routable, Debug, PartialEq)]
-enum Route {
-    #[route("/")]
-    Home {},
-    #[route("/blog/:id")]
-    Blog { id: i32 },
-}
+// #[derive(Clone, Routable, Debug, PartialEq)]
+// enum Route {
+//     #[route("/")]
+//     Home {},
+//     #[route("/blog/:id")]
+//     Blog { id: i32 },
+//     #[route("/:..route")]
+//     NotFound { route: Vec<String> },
+// }
 
 fn main() {
     // Init logger
@@ -18,47 +21,49 @@ fn main() {
     launch(App);
 }
 
-fn App() -> Element {
-    rsx! {
-        Router::<Route> {}
-    }
-}
+// #[allow(dead_code)]
+// fn NewApp() -> Element {
+//     rsx! {
+//         Router::<Route> {}
+//     }
+// }
 
-#[component]
-fn Blog(id: i32) -> Element {
-    rsx! {
-        Link { to: Route::Home {}, "Go to counter" }
-        "Blog post {id}"
-    }
-}
+// #[component]
+// fn Blog(id: i32) -> Element {
+//     rsx! {
+//         Link { to: Route::Home {}, "Go to counter" }
+//         "Blog post {id}"
+//     }
+// }
 
-#[component]
-fn NotFound() -> Element {
-    rsx! {
-        div { class: "flex flex-col items-center justify-center h-screen",
-            h1 { class: "text-3xl p-4", "404 Not Found" }
-        }
-    }
-}
+// #[component]
+// fn NotFound(route: Vec<String>) -> Element {
+//     let path = route.join("/");
+//     rsx! {
+//         div { class: "flex flex-col items-center justify-center h-screen",
+//             h1 { class: "text-3xl p-4", "404 page `/{path}` Not Found" }
+//         }
+//     }
+// }
 
-#[component]
-fn Home() -> Element {
-    let mut count = use_signal(|| 0);
+// #[component]
+// fn Home() -> Element {
+//     let mut count = use_signal(|| 0);
 
-    rsx! {
-        Link { to: Route::Blog { id: count() }, "Go to blog" }
-        div { class: "flex flex-col items-center justify-center h-screen",
-            h1 { class: "text-3xl p-4", "High-Five counter: {count}" }
-            button {
-                class: "text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800",
-                onclick: move |_| count += 1,
-                "Up high!"
-            }
-            button {
-                class: "focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900",
-                onclick: move |_| count -= 1,
-                "Down low!"
-            }
-        }
-    }
-}
+//     rsx! {
+//         Link { to: Route::Blog { id: count() }, "Go to blog" }
+//         div { class: "flex flex-col items-center justify-center h-screen",
+//             h1 { class: "text-3xl p-4", "High-Five counter: {count}" }
+//             button {
+//                 class: "text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800",
+//                 onclick: move |_| count += 1,
+//                 "Up high!"
+//             }
+//             button {
+//                 class: "focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900",
+//                 onclick: move |_| count -= 1,
+//                 "Down low!"
+//             }
+//         }
+//     }
+// }
---
+++ hackernews/src/ui/mod.rs
@@ -0,0 +1,28 @@
+#![allow(non_snake_case)]
+
+mod comments;
+mod stories;
+use comments::Comments;
+use dioxus::prelude::*;
+use stories::Stories;
+
+use crate::StoryData;
+
+#[derive(Debug, Clone)]
+pub enum CommentsState {
+    Unset,
+    Loading,
+    Loaded(StoryData),
+}
+
+pub fn App() -> Element {
+    use_context_provider(|| Signal::new(CommentsState::Unset));
+    rsx! {
+        main { class: "flex w-full h-full shadow-lg rounded-3xl",
+            section { class: "flex flex-col w-4/12 h-full pt-3 overflow-y-scroll bg-gray-50",
+                Stories {}
+            }
+            section { class: "flex flex-col w-8/12 px-4 bg-white rounded-r-3xl", Comments {} }
+        }
+    }
+}
